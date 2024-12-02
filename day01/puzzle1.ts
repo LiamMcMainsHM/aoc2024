10b75d18d78f9c9c39b0529da6b27a4229bd18e4
Liam McMains <liam.mcmains@henrymeds.com>
2024-12-02 09:52:27 -0600

    day 01

--- day01/puzzle1.ts
+++ day01/puzzle1.ts
@@ -1,12 +1,23 @@
 async function getInput(): Promise<string> {
-        const data = await Deno.readFile("input1.txt");
-        return new TextDecoder().decode(data);
-      }
+  const data = await Deno.readFile("input1.txt");
+  return new TextDecoder().decode(data);
+}
 
-      async function main() {
-        const input = await getInput();
-        console.log("Puzzle input:", input);
-        // Add your solution logic here
-      }
+async function main() {
+  const input = await getInput();
+  const lines = input.split("\n");
+  const firstColumn = lines.map((e) => e.split(" ").at(0));
+  const secondColumn = lines.map((e) => e.split(" ").at(3));
 
-      main();
+  const sortedFirstColumn = firstColumn.map(Number).sort();
+  const sortedSecondColumn = secondColumn.map(Number).sort();
+
+  let sum = 0;
+  for (let i = 0; i < sortedFirstColumn.length; i++) {
+    sum += Math.abs(sortedSecondColumn[i] - sortedFirstColumn[i]);
+  }
+
+  console.log(sum);
+}
+
+main();
--- day01/puzzle2.ts
+++ day01/puzzle2.ts
@@ -1,12 +1,38 @@
 async function getInput(): Promise<string> {
-        const data = await Deno.readFile("input2.txt");
-        return new TextDecoder().decode(data);
-      }
+  const data = await Deno.readFile("input1.txt");
+  return new TextDecoder().decode(data);
+}
 
-      async function main() {
-        const input = await getInput();
-        console.log("Puzzle input:", input);
-        // Add your solution logic here
-      }
+async function main() {
+  const input = await getInput();
+  const lines = input.split("\n");
+  const firstColumn = lines.map((e) => e.split(" ").at(0));
+  const secondColumn = lines.map((e) => e.split(" ").at(3));
 
-      main();
+  const sortedFirstColumn = firstColumn.map(Number).sort();
+  const sortedSecondColumn = secondColumn.map(Number).sort();
+
+  const frequencyFirstColumn = sortedFirstColumn.reduce((previous, current) => {
+    previous[current] = (previous[current] ?? 0) + 1;
+    return previous;
+  }, {} as Record<number, number>);
+
+  const frequencySecondColumn = sortedSecondColumn.reduce(
+    (previous, current) => {
+      previous[current] = (previous[current] ?? 0) + 1;
+      return previous;
+    },
+    {} as Record<number, number>,
+  );
+
+  console.log(
+    Object.entries(frequencyFirstColumn).reduce((previous, [stringKey]) => {
+      const key = Number(stringKey);
+      const secondColumnCount = frequencySecondColumn[key] ?? 0;
+
+      return previous + (key * secondColumnCount);
+    }, 0),
+  );
+}
+
+main();
